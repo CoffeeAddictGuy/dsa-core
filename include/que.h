1072149d0768aa0cd4748f019dba5d941f415015
CoffeeAddictGuy <richcrwo@gmail.com>
2025-12-11 15:45:41 +0300

    queue realization

---
+++ include/que.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+typedef struct {
+  int *data;
+  size_t readptr;
+  size_t writeptr;
+  size_t capacity;
+  size_t size;
+} Queue;
+
+typedef struct {
+  int code;
+  int result;
+} Result;
+
+Queue *q_init(size_t capacity);
+void q_write(Queue *queue, int value);
+Result q_read(Queue *queue);
+Result q_size(Queue *queue);
---
+++ src/que.c
@@ -0,0 +1,48 @@
+#include "que.h"
+
+Queue *q_init(size_t capacity) {
+  Queue *q = (Queue *)malloc(sizeof(Queue));
+  if (q == NULL) {
+    return NULL;
+  }
+
+  q->data = (int *)malloc(sizeof(int) * capacity);
+  q->readptr = 0;
+  q->writeptr = 0;
+  q->size = 0;
+  q->capacity = capacity;
+  return q;
+}
+
+void q_write(Queue *queue, int value) {
+  if ((queue->writeptr + 1) % queue->capacity == queue->readptr) {
+    return;
+  }
+  queue->data[queue->writeptr] = value;
+  queue->writeptr = (queue->writeptr + 1) % queue->capacity;
+  if (queue->size < queue->capacity) {
+    queue->size += 1;
+  }
+}
+
+Result q_read(Queue *queue) {
+  Result result;
+  if (queue->readptr == queue->writeptr) {
+    result = (Result){-1, 0};
+    return result;
+  }
+  result = (Result){0, queue->data[queue->readptr]};
+  queue->readptr = (queue->readptr + 1) % queue->capacity;
+  queue->size -= 1;
+  return result;
+}
+
+Result q_size(Queue *queue) {
+  Result result;
+  if (queue->size == 0) {
+    result = (Result){-1, 0};
+    return result;
+  }
+  result = (Result){0, queue->size};
+  return result;
+}
